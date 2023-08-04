86da82fc7f5da9acc4c4aace41f95ecdc0fe0595
miraz66 <mirajulislam0509@gmail.com>
2023-08-04 12:16:19 +0600

    dream touch blood

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,14 +10,14 @@ function Home() {
             src={MainPhoto}
             alt=""
           />
-          <h1 className="uppercase text-gray-700 text-4xl md:text-4xl lg:text-6xl font-bold text-center">
+          <h1 className="uppercase text-gray-700 text-3xl md:text-4xl lg:text-6xl font-bold text-center">
             Dream touch blood for life
           </h1>
           <p className="pt-3 2xl:pt-5 md:text-2xl text-center text-gray-600 font-semibold ">
             Elipur,Rupsha,Khulna
           </p>
 
-          <div className="flex justify-center md:mt-10 2xl:mt-14 mt-20">
+          <div className="flex justify-center md:mt-10 2xl:mt-14 mt-24">
             <button className="px-5 py-2 md:px-7 md:py-3 lg:px-10 lg:py-5 text-2xl md:text-3xl rounded-md text-gray-200 font-Poppins font-semibold bg-gradient-to-br from-gray-500 via-red-500 to-gray-500 ">
               View Details
             </button>
