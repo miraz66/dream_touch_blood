134516a57297ba0f74123a3b9c96679a0dd1518f
miraz66 <mirajulislam0509@gmail.com>
2023-08-18 01:39:48 +0600

    change

--- src/components/AutoSlider.jsx
+++ src/components/AutoSlider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Card from "./test/TestimonialCard";
 import DoctorImage from "../assets/image-Doctor.jpg";
 
