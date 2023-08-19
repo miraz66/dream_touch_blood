bc36c384fcc20c63b697da02b199fcc1572feb3d
miraz66 <mirajulislam0509@gmail.com>
2023-08-20 00:32:48 +0600

    setimage

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,9 +1,37 @@
 function Information() {
   return (
     <>
-      <div className="bg-gray-400">
+      <div className="bg-gray-50">
         <div className="bg-no-repeat bg-center bg-cover bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.1),rgba(0,0,0,0.2)),url('assets/appointment_female_bg.jpg')] h-[30rem]"></div>
-        <div className="">hello</div>
+        <div className="max-w-7xl mx-auto">
+          <div className="">
+            <div className="">
+              <h4 className="text-xl pb-1 text-gray-600 font-light">
+                Good To Know
+              </h4>
+              <hr className="bg-red-600 h-0.5 w-14" />
+              <h2 className="text-5xl font-medium text-gray-700">
+                Helpful Information
+              </h2>
+
+              <ul>
+                <li>
+                  Maintain a healthy iron level by eating iron rich foods.
+                </li>
+                <li>Drink an extra 16 oz. of water prior to your donation.</li>
+                <li>Avoid alcohol consumption before your blood donation.</li>
+                <li>
+                  Remember to bring the donor card or national ID/Passport.
+                </li>
+                <li>
+                  Finally, Try to get a good night sound sleep after donation.
+                </li>
+              </ul>
+            </div>
+            <div className=""></div>
+          </div>
+          <div className=""></div>
+        </div>
       </div>
     </>
   );
