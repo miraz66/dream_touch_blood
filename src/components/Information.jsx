edbc6c18e55121c35affa5d1f3c0c0b4bcc2f98c
miraz66 <mirajulislam0509@gmail.com>
2023-08-19 23:56:20 +0600

    Image

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,7 +2,9 @@ function Information() {
   return (
     <>
       <div className="">
-        <div className="bg-no-repeat bg-center bg-cover bg-[url('assets/testimony_feat_bg.jpg')]"></div>
+        <div className="bg-no-repeat bg-center bg-cover bg-[url('assets/testimony_feat_bg.jpg')] h-40">
+          hello
+        </div>
         <div className="">hello</div>
       </div>
     </>
