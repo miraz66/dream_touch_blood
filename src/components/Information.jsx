88143b979644b8bf0c3c3291366c2bafee7c960e
miraz66 <mirajulislam0509@gmail.com>
2023-08-20 00:22:43 +0600

    setimage

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,10 +1,8 @@
 function Information() {
   return (
     <>
-      <div className="">
-        <div className="bg-no-repeat bg-center bg-cover bg-[url('assets/testimony_feat_bg.jpg')] h-40">
-          hello
-        </div>
+      <div className="bg-gray-400">
+        <div className="bg-no-repeat bg-center bg-cover bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.1),rgba(0,0,0,0.2)),url('assets/appointment_female_bg.jpg')] h-[30rem]"></div>
         <div className="">hello</div>
       </div>
     </>
