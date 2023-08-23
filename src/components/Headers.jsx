b869dffe5b16fd057249288cfb798175a52e734a
miraz66 <mirajulislam0509@gmail.com>
2023-08-23 11:38:24 +0600

    windowRe

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -84,6 +84,7 @@ export default function Header() {
                         <NavLink
                           key={item.name}
                           to={item.href}
+                          onClick={() => window.reload()}
                           className={({ isActive }) => {
                             return (
                               "px-3 font-medium" +
