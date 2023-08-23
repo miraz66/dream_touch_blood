54fc3e7014889e920dd289ffe6feedd63c4e816e
miraz66 <mirajulislam0509@gmail.com>
2023-08-23 19:57:44 +0600

    All Animation

---
+++ src/components/AllPageScreen.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+export default function AllPageScreen({ name }) {
+  return (
+    <>
+      <div className="h-80 bg-no-repeat bg-center bg-cover bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.1),rgba(0,0,0,0.9)),url('assets/header-bg.jpg')]">
+        <div className="max-w-7xl mx-auto h-full text-white flex flex-col justify-center items-center">
+          <h2 className="text-4xl font-semibold uppercase tracking-widest">
+            {name}
+          </h2>
+          <div className="flex gap-2 text-lg font-semibold">
+            <a
+              className="after:content-[' '] after:bg-red-500 after:m-auto after:block after:w-0 after:h-[2px] after:ease-in after:duration-300 after:hover:w-full cursor-pointer"
+              href="/"
+            >
+              Home
+            </a>
+            <h2> / {name}</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+AllPageScreen.propTypes = {
+  name: PropTypes.string.isRequired,
+  css: PropTypes.string.isRequired,
+};
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,6 +11,7 @@ import Animation from "../components/Animation";
 import separator from "../assets/separator.png";
 import Button from "../utils/Button";
 import Footer from "../components/Footer";
+import AllPageScreen from "../components/AllPageScreen";
 
 function About() {
   // amimation library //
@@ -23,22 +24,7 @@ function About() {
         <Header />
 
         {/* ----About Home---- */}
-        <div className="h-80 bg-no-repeat bg-center bg-cover bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.1),rgba(0,0,0,0.9)),url('assets/header-bg.jpg')]">
-          <div className="max-w-7xl mx-auto h-full text-white flex flex-col justify-center items-center">
-            <h2 className="text-4xl font-semibold uppercase tracking-widest">
-              About us
-            </h2>
-            <div className="flex gap-2 text-lg font-semibold">
-              <a
-                className="after:content-[' '] after:bg-red-500 after:m-auto after:block after:w-0 after:h-[2px] after:ease-in after:duration-300 after:hover:w-full cursor-pointer"
-                href="/"
-              >
-                Home
-              </a>
-              <h2> / About us</h2>
-            </div>
-          </div>
-        </div>
+        <AllPageScreen name="About us" />
 
         {/* Details for company */}
         <DetailsCompany />
@@ -47,19 +33,20 @@ function About() {
         <VolunteerSection />
 
         {/* Achievements Section */}
-        <div className="bg-gray-50 py-28">
-          <div className="text-center pb-20">
-            <h1 className="pb-3 text-5xl tracking-wider text-gray-700 font-semibold uppercase">
+        <div className="bg-gray-50 py-10  w-screen   md:py-28">
+          <div className="text-center pb-20 px-3 md:px-5">
+            <h1 className="pb-3 text-2xl md:text-3xl lg:text-5xl tracking-wider text-gray-700 font-semibold uppercase">
               OUR ACHIEVEMENTS
             </h1>
 
-            <img className="mx-auto" src={separator} alt="" />
+            <img className="mx-auto" src={separator} alt="separator" />
 
-            <p className="text-gray-500 text-xl tracking-wide font-medium">
+            <p className="text-gray-500 lg:text-xl tracking-wide font-medium">
               We have been working since 1973 with a prestigious vision to
               helping patient to provide blood.
             </p>
           </div>
+
           {/* Counter Card */}
           <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 justify-between gap-5 2xl:gap-10 px-5 boxTarget">
             {CounterCardData.map(({ id, name, icon, counterCard }) => (
@@ -78,12 +65,12 @@ function About() {
 
         {/* Helping Section */}
         <div className="bg-gray-50 py-20">
-          <div className="max-w-7xl mx-auto grid grid-cols-3 gap-14">
-            <div className="col-span-2">
-              <h3 className="pb-5 text-4xl text-gray-700 font-semibold uppercase">
+          <div className="max-w-7xl mx-5 2xl:mx-auto lg:grid grid-cols-3 gap-14">
+            <div className="col-span-2 max-lg:pb-10">
+              <h3 className="pb-5 text-2xl md:text-3xl lg:text-4xl text-gray-700 font-semibold uppercase">
                 we are helping people from 40 years
               </h3>
-              <p className="text-lg">
+              <p className="text-sm md:text-base lg:text-lg">
                 You can give blood at any of our blood donation venues all over
                 the world. We have total sixty thousands donor centers and visit
                 thousands of other venues on various occasions.
@@ -92,7 +79,7 @@ function About() {
             <div className="">
               <Button
                 name="Request Appointment"
-                css="bg-red-600 px-6 py-4 text-xl text-white hover:bg-gray-700 ease-in-out duration-300"
+                css="bg-red-600 px-5 lg:px-6 py-3 lg:py-4 md:text-lg xl:text-xl text-white hover:bg-gray-700 ease-in-out duration-300"
               />
             </div>
           </div>
