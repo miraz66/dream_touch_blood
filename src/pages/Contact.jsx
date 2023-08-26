aee5521266b733d3a0d33a30c04538d4867d8bb3
miraz66 <mirajulislam0509@gmail.com>
2023-08-26 11:59:01 +0600

    contactRes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,15 +25,15 @@ function Contact() {
       {/* ----About Home---- */}
       <AllPageScreen name="Contact us" />
       {/* ----Contact Information----- */}
-      <div className="max-w-7xl mx-3 lg:mx-5 xl:mx-auto py-32">
+      <div className="max-w-7xl mx-3 lg:mx-5 2xl:mx-auto py-20 md:py-32">
         <h3 className="text-4xl font-medium text-gray-600">Contact us</h3>
         <div className="h-0.5 bg-gray-200 my-5">
           <p className="h-0.5 w-14 bg-red-600"></p>
         </div>
 
         <div className="">
-          <ul className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-            <li>
+          <ul className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-5 max-lg:gap-5">
+            <li className="max-lg:flex gap-3">
               <FontAwesomeIcon
                 className="bg-red-500 p-2 text-white"
                 icon={faHome}
@@ -92,12 +92,12 @@ function Contact() {
         </div>
       </div>
       {/* ----Communicate Section---- */}
-      <div className="bg-gray-100 py-28">
-        <div className="max-w-7xl mx-auto grid grid-cols-2 gap-10">
+      <div className="bg-gray-100 py-20 md:py-28">
+        <div className="max-w-7xl mx-3 lg:mx-5 2xl:mx-auto grid lg:grid-cols-2 gap-10">
           <div className="animationUp">
             <CommunicateForm />
           </div>
-          <div className="animationUp">
+          <div className="animationUp max-lg:h-[25rem]">
             <h2 className="text-3xl text-gray-600 font-semibold mb-4">
               Our Location
             </h2>
@@ -108,7 +108,7 @@ function Contact() {
           </div>
         </div>
       </div>
-      ={/* ----Footer Section---- */}
+      {/* ----Footer Section---- */}
       <Footer />
     </>
   );
